094ed37ffcd44ffd5685c62afb30202d7d653e95
Rayco Haex <raycoh1299@gmail.com>
2021-04-15 11:51:52 +0200

    🥤 Added Gulp pipeline

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+var minCss = require('gulp-minify-css')
+var rename = require('gulp-rename');
+const autoPrefixer = require('gulp-autoprefixer');
+
+/*
+* Gulp commands
+*/
+
+// $gulp style
+function style() {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }).on('error', sass.logError))
+        .pipe(autoPrefixer())
+        .pipe(minCss())
+        .pipe(rename({ extname: '.min.css'}))
+        .pipe(gulp.dest('./web/assets/css'))
+        .pipe(browserSync.stream());
+}
+
+// $gulp watch
+function watch() {
+    browserSync.init(null, {
+      proxy: 'rayco.test'
+    });
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./templates/**/*.twig').on('change', browserSync.reload);
+}
+
+/*
+* Init commands
+*/
+
+exports.style = style;
+exports.watch = watch;
