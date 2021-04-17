1916270ef50862722d0bf90c680deec3d5f2972b
Rayco Haex <raycoh1299@gmail.com>
2021-04-17 18:34:50 +0200

    🌐 localization integrated - not all translations

---
+++ translations/nl/site.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'years of experience' => 'jaar ervaring',
+    'websites built' => 'websites gebouwd',
+    'webapps built' => 'webapps gebouwd',
+    'Work' => 'werk',
+    'About Me' => "Over mij",
+    'My Fullstack Skillset' => "Mijn Fullstack Skillset",
+    'Projects I\'m especially proud of!' => 'Projecten waar ik éxtra trots op ben!',
+    'View all my projects' => "Bekijk al mijn projecten",
+    'Contact' => "Contact",
+    'For employers & internships' => "Voor werkgevers en stage-aanbod",
+    'For commissions and recruiters' => "Voor opdrachtgevers en recruiters"
+];
