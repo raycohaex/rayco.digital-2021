218875406e392e9ade96ff1c81c9ac1419573a20
Rayco Haex <raycoh1299@gmail.com>
2021-05-03 20:51:36 +0200

    🤘 + 💄 3D hand and template styling

--- translations/nl/site.php
+++ translations/nl/site.php
@@ -9,10 +9,11 @@ return [
     'My Fullstack Skillset' => "Mijn Fullstack Skillset",
     'Projects I\'m especially proud of!' => 'Projecten waar ik éxtra trots op ben!',
     'View all my projects' => "Bekijk al mijn projecten",
-    'Contact' => "Contact",
+    'let\'s build the future of web together' => "Laten we samen aan de toekomst van web bouwen",
     'For employers & internships' => "Voor werkgevers en stage-aanbod",
     'For commissions and recruiters' => "Voor opdrachtgevers en recruiters",
     'Home' => 'Home',
     'My Work' => 'Mijn Werk',
-    'Cases' => 'Projecten'
+    'Cases' => 'Projecten',
+    'Personal Resume - All rights reserved - designed & developed by Rayco Haex' => 'Personal Resume - Alle rechten voorbehouden - ontworpen en ontwikkeld door Rayco Haex'
 ];
